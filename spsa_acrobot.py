ac4f18e7d52110fe8ff0ad4694764f2f41e57e24
monika58 <42353806+monika58@users.noreply.github.com>
2019-05-13 16:04:29 +0530

    Update and rename spsa_acrobat.py to spsa_acrobot.py

--- spsa_acrobot.py
+++ spsa_acrobot.py
@@ -36,12 +36,7 @@ def normalize(X):
 def train_network(weights, envv, activation="sigmoid"):
     episodic_rewards = 0
     data_inputs = envv.reset()
-    #print(data_inputs)
-    # print(data_inputs)
     state = data_inputs
-    #state -= np.mean(state, axis=0)
-    #state /= np.std(state, axis=0)
-    #state = normalize(state)
     done = False
     while not done:
         r1 = []
@@ -62,13 +57,6 @@ def train_network(weights, envv, activation="sigmoid"):
         state = next_state
         episodic_rewards = episodic_rewards+ reward
     return episodic_rewards
-
-
-
-
-
-
-
 
 def delta(var):
     delta1 = []
@@ -125,57 +113,18 @@ def drop_out(p,h1):
 
 
 
-
-# delta(numpy.array(input_HL1_weights),1)
-
-#input_HL1_weights = np.random.uniform(low=-0.1, high=0.1,
-#                                      size=(4, HL1_neurons))
-# HL2_neurons = 10
-# HL1_HL2_weights = np.random.uniform(low=-0.1, high=0.1,
-#                                       size=(HL1_neurons, HL2_neurons))
-#output_neurons = 2
-#HL2_output_weights = np.random.uniform(low=-0.1, high=0.1, size=(HL1_neurons, output_neurons))
-# weights = np.array([input_HL1_weights,
-#                      HL1_HL2_weights,
-#                     HL2_output_weights])
-
-# HL1_neurons = 10
-# input_HL1_weights = np.random.randn(4, HL1_neurons)
-# HL2_neurons = 10
-# HL1_HL2_weights = np.random.randn(low=-0.1, high=0.1,
-#                                      size=(HL1_neurons, HL2_neurons))
-# output_neurons = 2
-# HL2_output_weights = np.random.randn(HL1_neurons, output_neurons)
-#weights = np.array([input_HL1_weights,
-                    # HL1_HL2_weights,
-#                    HL2_output_weights])
-
-
-#weights_old = weights
-#tau = 0.5
-start = time.time()
-
 final_weights = []
 final_rewards = []
 replications = 1
 env = gym.make("Acrobot-v1")
 env.seed(1)
-#from time import time
 
 for j in range(replications):
-    #np.random.seed(0)
     input_shape = 7
     output_dim = 3
-
     env1 = copy.deepcopy(env)
     env2 = copy.deepcopy(env)
     env3 = copy.deepcopy(env)
-    # num_episodes = 5
-    # alpha = 0.602
-    # gamma = 0.101
-    # a = 0.16
-    # c = 1
-    # A = 100
     a = 1.0
     c = 1.9
     A = 50
@@ -194,31 +143,19 @@ for j in range(replications):
     episode_id = 1
     env3 = wrappers.Monitor(env3, './acrobot_video1',resume=True, video_callable=lambda episode_id: True,force=True)
     for i in range(N):
-    #print(i)
         ak = a / (i + 1+A) ** alpha
         ck = c / (i + 1) ** gamma
-    # delta = np.random.choice([1,-1],p = [0.5,0.5],size = total_elem)
         delta2 = delta(weights)
-        #print("delta = ", delta2)
         weights_plus = perturb_weights(weights, delta2, ck)
         weights_minus = perturb_weights(weights, delta2, -ck)
-
-    # X1=env.reset()
-    # X2 = env1.reset()
         rewplus = train_network(weights_plus, env1, activation="tanh")
         rewminus = train_network(weights_minus, env2, activation="tanh")
         rewards1 = train_network(weights, env3, activation="tanh")
         rewards.append(rewards1)
         m = np.mean(rewards[:-10])
-        if m >195:
-            break
         mean_rewards.append(m)
         rewards_plus.append(rewplus)
         rewards_minus.append(rewminus)
-    #print("rewplus")
-    #print(rewplus)
-    #print("rewminus")
-    #print(rewminus)
         print('rep = ',j,'episode =' ,i, 'reward = ',rewards1)
         diff = (rewplus - rewminus) * ak
         weights = update_weights(weights, delta2, ck, diff)
@@ -229,7 +166,7 @@ for j in range(replications):
     #weights_old = weights
     #tau = 0.5
 
-    np.save('spsa_acrobat',final_rewards)
+    np.save('spsa_acrobot',final_rewards)
 
 
 avg_reward = np.zeros(5000)
@@ -239,49 +176,9 @@ for i in range(5000):
         sum = sum + final_rewards[k][i]
     avg_reward[i]=sum/len(final_rewards)
 
-avg_reward1=np.zeros(10)
 
-avg_reward=np.load('cartpole_avg_rew.npy')
-for i in range(1000):
-    if i%100 == 0:
-        avg_reward1[int(i/100)]=avg_reward[i]
-    #for k in range(len(final_rewards)):
-    #    sum = sum + final_rewards[k][i]
-    #avg_reward[i]=sum/len(final_rewards)
-
-plt.plot(avg_reward,'b')
-
-np.save('cartpole_avg_rew',avg_reward)
-
-plt.plot(avg_reward,'b')
-
-
-
-
-
-end = time.time()
-print(end - start)
-
-test_rewards=[]
-for i in range(100):
-    env3 = gym.make("CartPole-v0")
-    rewards3 = train_network(weights, env3, activation="sigmoid")
-    test_rewards.append(rewards3)
-plt.plot(np.arange(100),test_rewards)
-
-
-
-
-
-
-
-
-plt.plot(np.arange(N),rewards_plus)
-plt.plot(np.arange(N),mean_rewards)
 plt.plot(np.arange(N),rewards)
 np.save('mr',mean_rewards)
-np.save('rp',rewards_plus)
-np.save('rm',rewards_minus)
 np.save('r',rewards)
 smoothed_rewards = [np.mean(avg_reward[max(0,i-50):i+1]) for i in range(len(avg_reward))]
 smoothed_rewards1 = [np.mean(rewards[i-10:i+1]) for i in range(len(test_rewards))]
@@ -293,58 +190,3 @@ plt.title('SPSA based Policy Optimization')
 plt.xlabel('Number of Episodes')
 plt.ylabel('Smoothed Mean Rewards of last 10 episodes')
 plt.show()
-
-
-
-plt.figure(figsize=(12,8))
-plt.plot(smoothed_rewards,'r')
-#plt.plot(np.arange(N),rewards,'b')
-plt.title('SPSA based Policy Optimization')
-plt.xlabel('Number of Episodes')
-plt.ylabel('Episodic reward')
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-''''
-best yet configuration
---xavier
---a = 1
-c = 1.9
-A = 50
-N = 1000
-alpha = 1
-gamma = 1/6
-seed = 5
-HL1_neurons = 10
-sigmoid
-
-or
-
-a = 2.0
-c = 2.5
-A = 50
-N = 2000
-alpha = 1
-gamma = 1/6
-seed = 5
-discount_factor = 0.995
-HL1_neurons = 10
-
-
-
-'''
-
-
-
