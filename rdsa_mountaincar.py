6bb2d7661d485ca5559196ba590855f03e1c3bef
monika58 <42353806+monika58@users.noreply.github.com>
2019-05-13 15:58:56 +0530

    Update and rename rdsa_mc.py to rdsa_mountaincar.py

--- rdsa_mountaincar.py
+++ rdsa_mountaincar.py
@@ -54,11 +54,7 @@ def train_network(weights, envv, activation="relu", rendering=False):
     rew = []
     episodic_rewards = 0
     data_inputs = envv.reset()
-    # print(data_inputs)
     state = data_inputs
-    #state -= np.mean(state, axis=0)
-    #state /= np.std(state, axis=0)
-    #state = normalize(state)
     done = False
     t=0
     while not done:
@@ -83,12 +79,9 @@ def train_network(weights, envv, activation="relu", rendering=False):
         #print(r1)
         next_state, reward, done, info = envv.step(predicted_label)
         state = next_state
-        #print(state)
         episodic_rewards = episodic_rewards + reward
         t=t+1
         rew.append(reward)
-        #if(rendering):
-        #envv.render()
     return episodic_rewards,t,pos,vel,rew
 
 def delta(var, algo, epsilon):
@@ -128,25 +121,6 @@ def update_weights(var, delta2, ck, diff, epsilon, algo):
         var2.append(v)
     return var2
 
-''''
-def weight_initialization(input_shape,HL1_neurons,HL2_neurons, output_neurons, init_type):
-    input_HL1_weights=[]
-    HL1_HL2_weights = []
-    HL2_output_weights = []
-    if (init_type == "xavier"):
-        input_HL1_weights = np.random.randn(input_shape, HL1_neurons)/np.sqrt(input_shape / 2.)
-        HL1_HL2_weights = np.random.randn(HL1_neurons, HL2_neurons) / np.sqrt(HL1_neurons / 2.)
-        HL2_output_weights = np.random.randn(HL2_neurons, output_neurons)/np.sqrt(HL2_neurons / 2.)
-        #weights = np.array([input_HL1_weights, HL2_output_weights])
-    elif(init_type =="uniform"):
-        input_HL1_weights = np.random.uniform(low=-0.1, high=0.1,
-                                              size=(4, HL1_neurons))
-        HL2_output_weights = np.random.uniform(low=-0.1, high=0.1, size=(HL1_neurons, output_neurons))
-    weights = np.array([input_HL1_weights,HL1_HL2_weights, HL2_output_weights])
-    #b1 = np.zeros((1, H))
-    #b2 = np.zeros((1, H))
-    return weights
-'''
 def weight_initialization(input_shape,HL1_neurons, output_neurons, init_type):
     input_HL1_weights=[]
     HL2_output_weights = []
@@ -159,8 +133,6 @@ def weight_initialization(input_shape,HL1_neurons, output_neurons, init_type):
                                               size=(input_shape, HL1_neurons))/np.sqrt(input_shape / 2.)
         HL2_output_weights = np.random.uniform(low=-0.1, high=0.1, size=(HL1_neurons, output_neurons))/np.sqrt(HL1_neurons / 2.)
     weights = np.array([input_HL1_weights, HL2_output_weights])
-    #b1 = np.zeros((1, H))
-    #b2 = np.zeros((1, H))
     return weights
 
 
@@ -171,8 +143,6 @@ def drop_out(p,h1):
 
 
 
-
-# delta(numpy.array(input_HL1_weights),1)
 env = gym.make('MountainCarContinuous-v0')
 input_shape = env.observation_space.shape[0]
 output_dim = env.action_space.shape[0]
@@ -182,9 +152,7 @@ final_weights = []
 final_rewards = []
 rep = 1
 epsilon =0.01
-algo = "nonuniform"
-#HL2_neurons = 10
-#weights = weight_initialization(input_shape,HL1_neurons,HL2_neurons,output_dim,"xavier")
+algo = "nonuniform" #nonuniform for asymber bern , uniform for uniform bern
 for i in range(rep):
     alpha = 1
     gamma = 1 / 6
@@ -220,14 +188,6 @@ for i in range(rep):
         rewplus,tplus,pos1,vel1,rew1 = train_network(weights_plus, env1, activation="relu", rendering=False)
         rewminus,tminus,pos2,vel2,rew2 = train_network(weights_minus, env2, activation="relu",rendering=False)
         rewards1, t, pos3, vel3, rew3 = train_network(weights, env3, activation="relu", rendering=False)
-        # if(N<4990):
-        #     rewards1,t,pos3,vel3,rew3 = train_network(weights, env3, activation="relu",rendering=False)
-        # else:
-        #     rewards1, t, pos3, vel3, rew3 = train_network(weights, env3, activation="relu", rendering=True)
-        # pos_f.append(pos3)
-        # vel_f.append(vel3)
-        # rew_f.append(rew3)
-        # rewards.append(rewards1)
         episodic_length.append(t)
         m = np.mean(rewards[:-10])
         if m >195:
@@ -318,10 +278,6 @@ VELOCITY_MIN = -0.07
 VELOCITY_MAX = 0.07
 grid_size = 40
 positions = np.linspace(POSITION_MIN, POSITION_MAX, grid_size)
-# positionStep = (POSITION_MAX - POSITION_MIN) / grid_size
-# positions = np.arange(POSITION_MIN, POSITION_MAX + positionStep, positionStep)
-# velocityStep = (VELOCITY_MAX - VELOCITY_MIN) / grid_size
-# velocities = np.arange(VELOCITY_MIN, VELOCITY_MAX + velocityStep, velocityStep)
 velocities = np.linspace(VELOCITY_MIN, VELOCITY_MAX, grid_size)
 axis_x = []
 axis_y = []
@@ -338,26 +294,3 @@ ax.set_xlabel('Position')
 ax.set_ylabel('Velocity')
 ax.set_zlabel('Cost to go')
 ax.set_title('Episode %d' % (episode + 1))
-
-
-import matplotlib
-#matplotlib.use('GTK')
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-pos_f = np.load('rdsa_ab_1_mountaincar_pos.npy')
-vel_f = np.load('rdsa_ab_1_mountaincar_pos.npy')
-rew_f = np.load('rdsa_ab_1_mountaincar_rew1.npy')
-
-X=pos_f[0]
-Y=vel_f[0]
-Z=rew_f[0]
-#xs = np.arange(0, 5, 5/40.)
-#ys = np.arange(0, 5, 5/40.)
-xv, yv = np.meshgrid (X, Y)
-zv = np.zeros((999,999))
-ax = plt.gca(projection='3d')
-ax.contour(pos_f, vel_f, rew_f, zdir='x')
-plt.show()
-
-
